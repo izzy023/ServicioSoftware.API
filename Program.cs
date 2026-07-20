73b48e9e794b9224ce23df50f76269a5a6d9495c
izzy023 <ismaelgervaciofructuoso43@gmail.com>
2026-07-19 21:16:24 -0400

    Habilitar Swagger en producción

--- Program.cs
+++ Program.cs
@@ -36,7 +36,10 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsProduction())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseCors("MiPolitica");
 
